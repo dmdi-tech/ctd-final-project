5183ee96844bf27d129a88117a477929e666a6b5
d59692816@gmail.com <d59692816@gmail.com>
2026-03-10 20:43:54 -0400

    style fixes for player

--- src/shared/Player.jsx
+++ src/shared/Player.jsx
@@ -1,13 +1,14 @@
 import styled from 'styled-components';
-import mediaPlayer from '../assets/mediaplayer2.svg';
+import mediaPlayer from '../assets/mediaplayer.svg';
 import styles from '../shared/Player.module.css';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef } from 'react';
 
 const PlayerWrapper = styled.div`
     position: relative;
-    width:100%;
+    width: 60%;
+    max-width: 700px;
     margin: 0 auto;
-    top: calc(100% - 4px);
+    padding-top: 40px;
 `;
 
 const MediaPlayer = styled.img`
@@ -20,35 +21,25 @@ const MediaPlayer = styled.img`
 
 const ScreenOverlay = styled.div`
     position: absolute;
-    top: 48%;
+    top: 54%;
     left: 50%;
     transform: translate(-50%, -50%);
-
-    width: 60%;
-    height: 15%;
-
-    
+    width: 83%;
+    height: 41%;
     align-items: center;
     padding: 10px 15px;
-
     font-family: 'VT323', monospace;
     letter-spacing: 2px;
-
     background: linear-gradient(to bottom, #001a20, #000e12);
     border-radius: 4px;
-
     box-shadow:
         inset 0 0 8px #00eaff55,
         0 0 12px #00eaff33;
-
-
     pointer-events: none;
 `;
 
 const PlayingSong = styled.p`
-    display: flex;
-    flex-direction: row;
-    font-size: 25px;
+    font-size: 16px;
     font-style: italic;
     color: #00eaff;
     text-shadow:
